80ce6530da0d2762ce60a9a3ca44caa9101a8940
sujankhan187 <sussanekhan8@gmail.com>
2026-03-15 18:08:44 +0000

    change to t3 micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "demo" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "demo-server"
